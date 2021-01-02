7a6e6e94dcf68429551d6b30eb98a59695f90eb5
Thomas <thomasbiddlecombe@gmail.com>
2021-01-02 18:12:18 +0000

    fixin stuffff

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main(1);
+        Main main = new Main(2);
     }
 
     public Main(int mode){
@@ -16,10 +16,6 @@ public class Main {
             case 2:
                 FPSVisualiser.RCV();
                 break;
-
         }
-
-
-
     }
 }
