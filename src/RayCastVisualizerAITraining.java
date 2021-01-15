ef87379d4ca7e2c0ff22c24f499a60c660d7865e
freercurse <freerjoseph@yahoo.co.yk>
2021-01-15 18:05:58 +0000

    round system has been implemented, and the solider has 2 second invincibility frame, after each hit

--- src/RayCastVisualizerAITraining.java
+++ src/RayCastVisualizerAITraining.java
@@ -23,7 +23,7 @@ public class RayCastVisualizerAITraining {
     public RayCastVisualizerAITraining(){
         initPolygons();
         initSegments();
-        env = new Game(activeSegments, 10);
+        env = new Game(activeSegments, 5);
         Main.stats(0,1);
         loop();
     }
